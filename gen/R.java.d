e81f238e7399f8909d9508a78f3d51dbe78c4758
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-06-21 21:15:40 +0100

    Added basic dist contents

---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+/home/asandy/devel/p4a-dists/gen/org/kivy/android/R.java \
+ : /home/asandy/devel/p4a-dists/res/drawable-xhdpi/ic_launcher.png \
+/home/asandy/devel/p4a-dists/res/drawable-xxhdpi/ic_launcher.png \
+/home/asandy/devel/p4a-dists/res/layout/main.xml \
+/home/asandy/devel/p4a-dists/res/drawable-mdpi/ic_launcher.png \
+/home/asandy/devel/p4a-dists/res/drawable-hdpi/ic_launcher.png \
+/home/asandy/devel/p4a-dists/res/values/strings.xml \
+/home/asandy/devel/p4a-dists/bin/AndroidManifest.xml \
